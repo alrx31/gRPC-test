adff3bfd998a91b0b36d8d23f8b962258fd75711
Usov <usov.lesha2017@gmail.com>
2024-08-16 11:39:34 +0300

    init

---
+++ GrpcExample/Services/GreeterService.cs
@@ -0,0 +1,34 @@
+using Grpc.Core;
+using GrpcExample;
+
+namespace GrpcExample.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override async Task SayHelloStream(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+    {
+        
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                await responseStream.WriteAsync(new HelloReply
+                {
+                    Message = "Hello " + request.Name+" "+DateTime.UtcNow
+                });
+            }
+        }
+    }
+
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcExample;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7235");
+            var client = new Greeter.GreeterClient(channel);
+
+            using var call = client.SayHelloStream();
+
+            _ = Task.Run(async () =>
+            {
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine("Greeting: " + response.Message);
+                }
+            });
+
+            while (true ){
+                var result = Console.ReadLine();
+                if (string.IsNullOrEmpty(result))
+                {
+                    break;
+                }
+                await call.RequestStream.WriteAsync(new HelloRequest { Name = result });
+            }
+
+            await call.RequestStream.CompleteAsync();
+
+            //Console.WriteLine("Greeting: " + reply.Message);
+            Console.ReadKey();
+
+        }
+
+    }
+}
